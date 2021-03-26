fedca675e8d2fb05a6affafe0b2ed2b1545043af
Master-Snack <sonphukl@gmail.com>
2021-03-26 13:50:04 +0700

    predict model

--- titanic/base/views.py
+++ titanic/base/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
-
+import pickle
 
 def home(request):
     return render(request, 'index.html')
+
+def getPredictions(pclass, sex, age, sibsp, parch, fare, C, Q, S):
+    model = pickle.load(open('model.sav', 'rb'))
+    scaled = pickle.load(open('scler.sav', 'rb'))
+
+    prediction = model.predict(scaled.transform([
+        [pclass, sex, age, sibsp, parch, fare, C, Q, S]
+    ]))
+
+    if prediction == 0:
+        return 'no'
+    elif prediction == 1:
+        return 'yes'
+    else:
+        return 'error'
+
+
+
