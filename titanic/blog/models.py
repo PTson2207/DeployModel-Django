2940ca72d1a62bba59e1893646c8b94df5d10cea
Master-Snack <sonphukl@gmail.com>
2021-03-28 21:34:12 +0700

    fix database Post

--- titanic/blog/models.py
+++ titanic/blog/models.py
@@ -3,7 +3,11 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     date = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100)
     pclass = models.IntegerField(max_length=50)
     sex = models.BooleanField()
     age = models.IntegerField()
     sibsp = models.IntegerField()
+
+    def __str__(self):
+        return self.pclass
