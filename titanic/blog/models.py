7fb0a8b3d4cabacc41b2f2420c45681d7c8d5346
Master-Snack <sonphukl@gmail.com>
2021-03-28 14:30:31 +0700

    create sql/python manage.py makemigrations blog/ python...migrate

--- titanic/blog/models.py
+++ titanic/blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    date = models.DateTimeField(auto_now_add=True)
+    pclass = models.IntegerField(max_length=50)
+    sex = models.BooleanField()
+    age = models.IntegerField()
+    sibsp = models.IntegerField()
